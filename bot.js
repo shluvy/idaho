eee1b90376aa34d2a55937c8a6634d5133ab6fac
shluvy <53138083+shluvy@users.noreply.github.com>
2019-08-05 14:43:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,10 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require("discord.js");
+var bot = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+bot.on("ready", () => {
+  console.log("You are connected to " + bot.guilds.size + " servers!");
+  bot.user.setGame("chla9 chla9", "https://www.twitch.tv/monstercat")
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
 
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
